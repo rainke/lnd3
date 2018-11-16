bb8a9f557c3b35ababa20f408e05b18f728197d8
hepeng <hepeng@gridsum.com>
2018-11-16 19:32:09 +0800

    add drag

--- src/test.js
+++ src/test.js
@@ -2,13 +2,19 @@ var Test;
 (function (Test) {
     var w = 600, h = 600;
     var svg = d3.select('#root').append('svg').attr('width', w).attr('height', h);
+    var x = d3.scaleLinear()
+        .domain([0, 10])
+        .range([0, w]);
+    console.log(x.invert(3));
     Test.g = svg.append('g');
     Test.brush = d3.brush()
         .on('start', function () {
-        Test.brush.move(Test.g, [[0, 0], [80, 80]]);
+        // g.transition().call(brush.move,  [[0, 0], [80, 80]])
+        // brush.move(g.transition(), [[0, 0], [80, 80]])
     })
         .on('brush', function () {
         console.log('brush');
+        console.log(d3.event.selection);
     })
         .on('end', function () {
         console.log('end');
--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,19 @@
 namespace Test {
     const w = 600, h = 600;
     const svg = d3.select('#root').append('svg').attr('width', w).attr('height', h);
+    const x = d3.scaleLinear()
+    .domain([0, 10])
+    .range([0, w]);
+    console.log(x.invert(3))
     export const g = svg.append('g');
     export const brush = d3.brush()
         .on('start', function() {
-            brush.move(g, [[0, 0], [80, 80]])
+            // g.transition().call(brush.move,  [[0, 0], [80, 80]])
+            // brush.move(g.transition(), [[0, 0], [80, 80]])
         })
         .on('brush', function(){
             console.log('brush');
+            console.log(d3.event.selection)
         })
         .on('end', function(){
             console.log('end');
